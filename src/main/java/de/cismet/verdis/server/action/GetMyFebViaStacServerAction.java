b7c6e516ebe609fd2df8d63702699c19c1630325
JeanAtCismet <jean.ruiz@cismet.de>
2018-07-18 09:54:11 +0200

    issue #31 getMyFEB preparations

--- src/main/java/de/cismet/verdis/server/action/GetMyFebViaStacServerAction.java
+++ src/main/java/de/cismet/verdis/server/action/GetMyFebViaStacServerAction.java
@@ -14,9 +14,13 @@ package de.cismet.verdis.server.action;
 
 import Sirius.server.middleware.interfaces.domainserver.MetaService;
 import Sirius.server.middleware.interfaces.domainserver.MetaServiceStore;
+import Sirius.server.newuser.User;
 
 import de.cismet.cids.server.actions.ServerAction;
 import de.cismet.cids.server.actions.ServerActionParameter;
+import de.cismet.cids.server.actions.UserAwareServerAction;
+import de.cismet.connectioncontext.ConnectionContext;
+import de.cismet.connectioncontext.ConnectionContextStore;
 
 /**
  * DOCUMENT ME!
@@ -25,7 +29,7 @@ import de.cismet.cids.server.actions.ServerActionParameter;
  * @version  $Revision$, $Date$
  */
 @org.openide.util.lookup.ServiceProvider(service = ServerAction.class)
-public class GetMyFebViaStacServerAction implements MetaServiceStore, ServerAction {
+public class GetMyFebViaStacServerAction implements MetaServiceStore, UserAwareServerAction, ServerAction, ConnectionContextStore {
 
     //~ Static fields/initializers ---------------------------------------------
 
@@ -34,9 +38,24 @@ public class GetMyFebViaStacServerAction implements MetaServiceStore, ServerActi
 
     //~ Instance fields --------------------------------------------------------
 
+
+    private User user;
+
     private MetaService metaService;
 
+    private ConnectionContext connectionContext = ConnectionContext.createDummy();
+
     //~ Methods ----------------------------------------------------------------
+
+    @Override
+    public User getUser() {
+        return user;
+    }
+
+    @Override
+    public void setUser(final User user) {
+        this.user = user;
+    }
 
     @Override
     public void setMetaService(final MetaService metaService) {
@@ -49,9 +68,78 @@ public class GetMyFebViaStacServerAction implements MetaServiceStore, ServerActi
     }
 
     @Override
-    public Object execute(final Object o, final ServerActionParameter... saps) {
-        LOG.fatal("GetMyFebViaStacServerAction");
-        return "Gute Reise.GetMyFebViaStacServerAction";
+    public void initWithConnectionContext(final ConnectionContext connectionContext) {
+        this.connectionContext = connectionContext;
+    }
+
+    @Override
+    public ConnectionContext getConnectionContext() {
+        return connectionContext;
+    }        
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @version $Revision$, $Date$
+     */
+    public enum Parameter {
+
+        BODY
+    }
+    
+    public enum Body {
+
+        STRING, BYTE_ARRAY
+    }
+    
+    //~ Methods ----------------------------------------------------------------
+
+    @Override
+    public Object execute(final Object object, final ServerActionParameter... params) {
+        Body body = Body.BYTE_ARRAY;
+        try {
+            if (params != null) {
+                for (final ServerActionParameter sap : params) {
+                    if (sap.getKey().equals(Parameter.BODY.toString())) {
+                        body = Body.valueOf((String)sap.getValue());
+                    }
+                }
+            }
+
+            final String stac;
+            if (object == null) {
+                throw new Exception("body is null");
+            } else if (body == null) {
+                throw new Exception("body-type parameter is null");
+            } else {
+                switch (body) {
+                    case BYTE_ARRAY: {
+                        stac = new String((byte[])object);
+                    }
+                    break;
+                    case STRING: {
+                        stac = (String)object;
+                    }
+                    break;
+                    default: {
+                        stac = null;
+                    }
+                }
+            }
+            
+            if (stac == null) {
+                throw new Exception("STAC is null");
+            } else {
+                return createReport(stac);
+            }
+        } catch (final Exception ex) {
+            LOG.error(ex, ex);
+            return ex;
+        }   
+    }
+    
+    private byte[] createReport(final String stac) throws Exception {
+        return null;
     }
 
     @Override
