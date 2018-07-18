be17a2d0a5aa0c33ea118b27e6d66e6fd6b5cfff
JeanAtCismet <jean.ruiz@cismet.de>
2018-07-18 09:36:38 +0200

    issue #32 adding body type parameter

--- src/main/java/de/cismet/verdis/server/action/EBReportServerAction.java
+++ src/main/java/de/cismet/verdis/server/action/EBReportServerAction.java
@@ -23,6 +23,8 @@ import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
+import java.math.BigInteger;
+
 import java.util.Base64;
 import java.util.Properties;
 
@@ -84,11 +86,23 @@ public class EBReportServerAction implements UserAwareServerAction, MetaServiceS
      *
      * @version  $Revision$, $Date$
      */
+    public static enum Body {
+
+        //~ Enum constants -----------------------------------------------------
+
+        BYTE_ARRAY, STRING, INTEGER
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @version  $Revision$, $Date$
+     */
     public enum Parameter {
 
         //~ Enum constants -----------------------------------------------------
 
-        TYPE, MAP_FORMAT, MAP_SCALE, ABLUSSWIRKSAMKEIT, HINTS
+        TYPE, MAP_FORMAT, MAP_SCALE, ABLUSSWIRKSAMKEIT, HINTS, BODY
     }
 
     //~ Instance fields --------------------------------------------------------
@@ -143,18 +157,20 @@ public class EBReportServerAction implements UserAwareServerAction, MetaServiceS
     }
 
     @Override
-    public Object execute(final Object body, final ServerActionParameter... params) {
-        final Integer kassenzeichenId = (Integer)body;
-        Type type = null;
-        MapFormat mapFormat = null;
-        String hints = null;
+    public Object execute(final Object object, final ServerActionParameter... params) {
+        Body body = Body.INTEGER;
+        Type type = Type.FLAECHEN;
+        MapFormat mapFormat = MapFormat.A4LS;
+        String hints = "";
+        Boolean abflusswirksamkeit = false;
         Double scaleDenominator = null;
-        Boolean abflusswirksamkeit = null;
 
         try {
             if (params != null) {
                 for (final ServerActionParameter sap : params) {
                     if (sap.getKey().equals(Parameter.TYPE.toString())) {
+                        body = Body.valueOf((String)sap.getValue());
+                    } else if (sap.getKey().equals(Parameter.TYPE.toString())) {
                         type = Type.valueOf((String)sap.getValue());
                     } else if (sap.getKey().equals(Parameter.MAP_FORMAT.toString())) {
                         mapFormat = MapFormat.valueOf((String)sap.getValue());
@@ -168,42 +184,86 @@ public class EBReportServerAction implements UserAwareServerAction, MetaServiceS
                 }
             }
 
-            try {
-                final Properties properties = ServerResourcesLoader.getInstance()
-                            .loadProperties(VerdisServerResources.EB_REPORT_PROPERTIES.getValue());
-
-                final Properties cmdProperties = new Properties();
-                final InputStream inputStream = new FileInputStream((String)properties.get("ebGeneratorCmdProperties"));
-                cmdProperties.load(inputStream);
-
-                final String abflusswirksamkeitFlag;
-                if (Boolean.TRUE.equals(abflusswirksamkeit)) {
-                    abflusswirksamkeitFlag = (String)cmdProperties.getProperty("abflusswirksamkeitFlag");
-                } else {
-                    abflusswirksamkeitFlag = "";
+            final Integer kassenzeichenId;
+            if (object == null) {
+                throw new Exception("body is null");
+            } else if (body == null) {
+                throw new Exception("body-type parameter is null");
+            } else {
+                switch (body) {
+                    case BYTE_ARRAY: {
+                        kassenzeichenId = new BigInteger((byte[])object).intValue();
+                    }
+                    break;
+                    case INTEGER: {
+                        kassenzeichenId = (Integer)object;
+                    }
+                    break;
+                    case STRING: {
+                        kassenzeichenId = Integer.parseInt((String)object);
+                    }
+                    break;
+                    default: {
+                        kassenzeichenId = null;
+                    }
                 }
-
-                final String ebGeneratorCmd = cmdProperties.getProperty("cmd")
-                            .replaceAll("<callserverUrl>", (String)cmdProperties.get("callserverUrl"))
-                            .replaceAll("<compressionFlag>", (String)cmdProperties.get("compressionFlag"))
-                            .replaceAll("<user>", (String)cmdProperties.get("user"))
-                            .replaceAll("<group>", (String)cmdProperties.get("group"))
-                            .replaceAll("<domain>", (String)cmdProperties.get("domain"))
-                            .replaceAll("<password>", (String)cmdProperties.get("password"))
-                            .replaceAll("<kassenzeichenId>", String.valueOf(kassenzeichenId))
-                            .replaceAll("<type>", (type != null) ? type.name() : "_null_")
-                            .replaceAll("<mapFormat>", (mapFormat != null) ? mapFormat.name() : "_null_")
-                            .replaceAll("<hints>", hints)
-                            .replaceAll("<scaleDenominator>", String.valueOf(scaleDenominator))
-                            .replaceAll("<abflusswirksamkeitFlag>", String.valueOf(abflusswirksamkeitFlag));
-                final String response = executeCmd(ebGeneratorCmd);
-                return Base64.getMimeDecoder().decode(response);
-            } finally {
             }
+            return createReport(kassenzeichenId, type, mapFormat, hints, scaleDenominator, abflusswirksamkeit);
         } catch (final Exception ex) {
             LOG.error(ex, ex);
             return ex;
         }
+    }
+
+    /**
+     * DOCUMENT ME!
+     *
+     * @param   kassenzeichenId     DOCUMENT ME!
+     * @param   type                DOCUMENT ME!
+     * @param   mapFormat           DOCUMENT ME!
+     * @param   hints               DOCUMENT ME!
+     * @param   scaleDenominator    DOCUMENT ME!
+     * @param   abflusswirksamkeit  DOCUMENT ME!
+     *
+     * @return  DOCUMENT ME!
+     *
+     * @throws  Exception  DOCUMENT ME!
+     */
+    private byte[] createReport(final Integer kassenzeichenId,
+            final Type type,
+            final MapFormat mapFormat,
+            final String hints,
+            final Double scaleDenominator,
+            final Boolean abflusswirksamkeit) throws Exception {
+        final Properties properties = ServerResourcesLoader.getInstance()
+                    .loadProperties(VerdisServerResources.EB_REPORT_PROPERTIES.getValue());
+
+        final Properties cmdProperties = new Properties();
+        final InputStream inputStream = new FileInputStream((String)properties.get("ebGeneratorCmdProperties"));
+        cmdProperties.load(inputStream);
+
+        final String abflusswirksamkeitFlag;
+        if (Boolean.TRUE.equals(abflusswirksamkeit)) {
+            abflusswirksamkeitFlag = (String)cmdProperties.getProperty("abflusswirksamkeitFlag");
+        } else {
+            abflusswirksamkeitFlag = "";
+        }
+
+        final String ebGeneratorCmd = cmdProperties.getProperty("cmd")
+                    .replaceAll("<callserverUrl>", (String)cmdProperties.get("callserverUrl"))
+                    .replaceAll("<compressionFlag>", (String)cmdProperties.get("compressionFlag"))
+                    .replaceAll("<user>", (String)cmdProperties.get("user"))
+                    .replaceAll("<group>", (String)cmdProperties.get("group"))
+                    .replaceAll("<domain>", (String)cmdProperties.get("domain"))
+                    .replaceAll("<password>", (String)cmdProperties.get("password"))
+                    .replaceAll("<kassenzeichenId>", String.valueOf(kassenzeichenId))
+                    .replaceAll("<type>", (type != null) ? type.name() : "_null_")
+                    .replaceAll("<mapFormat>", (mapFormat != null) ? mapFormat.name() : "_null_")
+                    .replaceAll("<hints>", hints)
+                    .replaceAll("<scaleDenominator>", String.valueOf(scaleDenominator))
+                    .replaceAll("<abflusswirksamkeitFlag>", String.valueOf(abflusswirksamkeitFlag));
+        final String response = executeCmd(ebGeneratorCmd);
+        return Base64.getMimeDecoder().decode(response);
     }
 
     @Override
