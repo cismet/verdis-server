895b1292966242d83d95bc945ed4c67a6438172b
Jean-Michel Ruiz <jean.ruiz@cismet.de>
2021-09-02 13:14:43 +0200

    issue #43 adding messageconfigjson for more flexible notificationhandling

---
+++ src/main/java/de/cismet/verdis/server/json/MessageConfigJson.java
@@ -0,0 +1,39 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.cismet.verdis.server.json;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   jruiz
+ * @version  $Revision$, $Date$
+ */
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(callSuper = false)
+public class MessageConfigJson extends AbstractJson {
+
+    //~ Instance fields --------------------------------------------------------
+
+    private String cmdTemplate;
+    private String topic;
+    private String messageTemplateFile;
+}
