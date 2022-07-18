ae5d216e830f4389ff56b71855827797d310f04e
Jean-Michel Ruiz <jean.ruiz@cismet.de>
2022-07-18 15:03:40 +0200

    fixed #256

---
+++ src/main/java/de/cismet/verdis/commons/constants/AenderungsanfrageStatusPropertyConstants.java
@@ -0,0 +1,30 @@
+/***************************************************
+*
+* cismet GmbH, Saarbruecken, Germany
+*
+*              ... and it just works.
+*
+****************************************************/
+package de.cismet.verdis.commons.constants;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @author   jruiz
+ * @version  $Revision$, $Date$
+ */
+public final class AenderungsanfrageStatusPropertyConstants extends PropertyConstants {
+
+    //~ Static fields/initializers ---------------------------------------------
+
+    public static final String NAME = "name";
+    public static final String SCHLUESSEL = "schluessel";
+
+    //~ Constructors -----------------------------------------------------------
+
+    /**
+     * Creates a new AenderungsanfrageStatusPropertyConstants object.
+     */
+    AenderungsanfrageStatusPropertyConstants() {
+    }
+}
